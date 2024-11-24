ef08531e224e7fb52ec9c4b2e2256d2c0f3f6296
Xzihnago <xzihnago@gmail.com>
2024-11-24 14:13:18 +0800

    Remove dev route

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,11 +3,8 @@ import middleware from "@xzihnago/middleware";
 import userRoutes from "@/api/user/routes";
 import donorRecordRoutes from "@/api/donorRecords/routes";
 import donorRelationshipRoutes from "@/api/donorRelationship/routes";
-import devRoutes from "./dev";
 
 const router = express.Router();
-
-router.use("/dev", devRoutes);
 
 router.use("/user", userRoutes);
 router.use("/donorRecords", donorRecordRoutes);
