292f62f4ef06ad73e2546550adcb1245973065c7
xzihnago <xzihnago@gmail.com>
2024-12-15 13:08:06 +0800

    chore(db): add Prisma migration record

---
+++ prisma/migrations/20241215050608_init/migration.sql
@@ -0,0 +1,45 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "admin" BOOLEAN NOT NULL DEFAULT false,
+    "username" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Donor" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "superiorId" TEXT,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Donor_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "DonationRecord" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "donorId" TEXT NOT NULL,
+    "amount" INTEGER NOT NULL,
+
+    CONSTRAINT "DonationRecord_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Donor_name_key" ON "Donor"("name");
+
+-- AddForeignKey
+ALTER TABLE "Donor" ADD CONSTRAINT "Donor_superiorId_fkey" FOREIGN KEY ("superiorId") REFERENCES "Donor"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "DonationRecord" ADD CONSTRAINT "DonationRecord_donorId_fkey" FOREIGN KEY ("donorId") REFERENCES "Donor"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
