8d1ff9ed7cc0b5f573f383d9a563b04d41755eef
xzihnago <xzihnago@gmail.com>
2025-04-02 17:56:18 +0800

    chore(deps): bump dependencies

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,7 @@ app.set("view engine", "ejs");
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(express.json({ limit: "1mb" }));
 
-app.use(middleware.routeLog);
+app.use(middleware.routeLogger);
 app.use(middleware.responseHandler);
 app.use("/api", router);
 app.use("/", routerWWW);
