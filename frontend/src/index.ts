7e66ee895a543c67a52e40bb804760c3843daa3c
xzihnago <xzihnago@gmail.com>
2025-05-14 19:56:13 +0800

    Fix aria-hidden warning

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -2,3 +2,9 @@ import "./p-login";
 import "./p-search";
 import "./p-relation";
 import "./p-upload";
+
+document.addEventListener("hide.bs.modal", () => {
+  if (document.activeElement) {
+    (document.activeElement as HTMLElement).blur();
+  }
+});
