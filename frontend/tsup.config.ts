42dda8e61f3783bacfbdb03d4d2d365c232c45b9
xzihnago <xzihnago@gmail.com>
2025-07-09 08:40:19 +0800

    Move static assets to frontend sub-repo

--- frontend/tsup.config.ts
+++ frontend/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   format: "esm",
   entry: ["src/index.ts"],
-  outDir: "../public/scripts",
+  outDir: "./public/scripts",
   clean: true,
   minify: true,
   treeshake: true,
